4685e09bab5cbb82477829da0702287815e39854
Ben Trott <ben@sixapart.com>
2007-09-20 05:39:16 +0000

    Committing first version of Elsewhere plugin.

---
+++ plugins/Elsewhere/elsewhere.pl
@@ -0,0 +1,210 @@
+# $Id$
+
+package MT::Plugin::Elsewhere;
+use strict;
+use warnings;
+use base qw( MT::Plugin );
+
+use Carp qw( croak );
+use WWW::SocialNetworks;
+
+our $VERSION = '1.0';
+
+my $plugin = __PACKAGE__->new({
+    id          => 'elsewhere',
+    name        => 'Elsewhere',
+    version     => $VERSION,
+    author_name => 'Benjamin Trott',
+    author_link => 'http://www.sixapart.com/',
+    description => 'Elsewhere allows you to link to your other profiles/websites online. You can add other websites in your Edit Profile screen.',
+});
+MT->add_plugin( $plugin );
+
+sub init_registry {
+    my $plugin = shift;
+    $plugin->registry( {
+        applications => {
+            cms => {
+                methods => {
+                    other_profiles => sub {
+                        $plugin->other_profiles( @_ ),
+                    },
+                    add_other_profile => sub {
+                        $plugin->add_other_profile( @_ ),
+                    },
+                    remove_other_profile => sub {
+                        $plugin->remove_other_profile( @_ ),
+                    },
+                    embed_profiles => sub {
+                        $plugin->embed_profiles( @_ ),
+                    }
+                },
+            },
+        },
+        callbacks => {
+            'MT::App::CMS::template_output.edit_author' => sub {
+                $plugin->insert_profile_link( @_ );
+            },
+            'MT::App::CMS::template_output.other_profiles' => sub {
+                $plugin->insert_profile_link( @_ );
+            },
+        },
+        tags => {
+            function => {
+                OtherProfileVar => \&tag_OtherProfileVar,
+            },
+            block => {
+                OtherProfiles => \&tag_OtherProfiles,
+            },
+        },
+    } );
+}
+
+sub tag_OtherProfiles {
+    my( $ctx, $args, $cond ) = @_;
+    my $author = $args->{author}
+        or return $ctx->error( 'author argument is required' );
+    my( $user ) = MT::Author->search({ name => $author })
+        or return $ctx->error( 'no such user ' . $author );
+    my $out = "";
+    my $builder = $ctx->stash( 'builder' );
+    my $tokens = $ctx->stash( 'tokens' );
+    for my $profile ( @{ $user->other_profiles } ) {
+        $ctx->stash( other_profile => $profile );
+        $out .= $builder->build( $ctx, $tokens, $cond );
+    }
+    return $out;
+}
+
+sub tag_OtherProfileVar {
+    my( $ctx, $args ) = @_;
+    my $profile = $ctx->stash( 'other_profile' )
+        or return $ctx->error( 'No profile defined in ProfileVar' );
+    my $var = $args->{name} || 'uri';
+    return defined $profile->{ $var } ? $profile->{ $var } : '';
+}
+
+sub insert_profile_link {
+    my $plugin = shift;
+    my( $cb, $app, $html_ref, $param, $tmpl ) = @_;
+    my $base_uri = $app->uri;
+    $$html_ref =~ s/(<li>.*?Permissions<\/a><\/li>)/$1\n<li><a href="$base_uri?__mode=other_profiles">Other Profiles<\/a><\/li>/;
+}
+
+sub other_profiles {
+    my $plugin = shift;
+    my( $app ) = @_;
+    my $tmpl = $plugin->load_tmpl( 'other_profiles.tmpl' );
+    my $networks = WWW::SocialNetworks->all;
+    return $app->build_page( $tmpl, {
+        id          => $app->user->id,
+        profiles    => $app->user->other_profiles,
+        networks    => [
+            map { +{
+                type    => $_,
+                label   => $networks->{ $_ }[ 0 ],
+            } } sort keys %$networks,
+        ],
+    } );
+}
+
+sub add_other_profile {
+    my $plugin = shift;
+    my( $app ) = @_;
+    $app->validate_magic or return;
+
+    my( $ident, $uri, $label, $type );
+    if ( $type = $app->param( 'profile_type' ) ) {
+        my $network = WWW::SocialNetworks->get( $type )
+            or croak "Unknown network $type";
+        $label = $network->name . ' Profile';
+        $ident = $app->param( 'profile_id' );
+        $uri = $network->uri_for( $ident );
+    } else {
+        $ident = $uri = $app->param( 'profile_uri' );
+        $label = $app->param( 'profile_label' );
+        $type = 'website';
+    }
+
+    $app->user->add_profile({
+        type    => $type,
+        ident   => $ident,
+        label   => $label,
+        uri     => $uri,
+    });
+
+    return $app->redirect( $app->uri( mode => 'other_profiles' ) );
+}
+
+sub remove_other_profile {
+    my $plugin = shift;
+    my( $app ) = @_;
+    $app->validate_magic or return;
+
+    if ( my $ident = $app->param('ident') ) {
+        ( my( $type ), $ident ) = split /:/, $ident;
+        $app->user->remove_profile( $type, $ident );
+    }
+    
+    return $app->redirect( $app->uri( mode => 'other_profiles' ) );
+}
+
+sub embed_profiles {
+    my $plugin = shift;
+    my( $app ) = @_;
+    my $name = $app->user->name;
+    my $html = <<HTML;
+<div class="widget-elsewhere widget">
+    <h3 class="widget-header">Elsewhere</h3>
+    <link rel="stylesheet" type="text/css" href="<mt:staticwebpath>plugins/Elsewhere/css/networks.css" />
+    <ul id="elsewhere-profiles" class="network">
+    <mt:otherprofiles author="$name">
+        <li>
+            <a href="<mt:otherprofilevar name="uri" escape="html">" rel="me" class="<mt:otherprofilevar name="type">"><mt:otherprofilevar name="label" escape="html"></a>
+        </li>
+    </mt:otherprofiles>
+    </ul>
+</div>
+HTML
+    my $tmpl = $plugin->load_tmpl( 'embed.tmpl' );
+    return $app->build_page( $tmpl, { embed => $html } );
+}
+
+MT::Author->install_meta({
+    columns => [
+        'other_profiles',
+    ],
+});
+
+sub MT::Author::other_profiles {
+    my $user = shift;
+    my( $type ) = @_;
+    require Storable;
+    my $profiles = Storable::thaw( $user->meta( 'other_profiles' ) ) || [];
+    return $type ?
+        [ grep { $_->{type} eq $type } @$profiles ] :
+        $profiles;
+}
+
+sub MT::Author::add_profile {
+    my $user = shift;
+    my( $profile ) = @_;
+    my $profiles = $user->other_profiles;
+    push @$profiles, $profile;
+    require Storable;
+    $user->meta( other_profiles => Storable::nfreeze( $profiles ) );
+    $user->save;
+}
+
+sub MT::Author::remove_profile {
+    my $user = shift;
+    my( $type, $ident ) = @_;
+    my $profiles = [ grep {
+        $_->{type} ne $type || $_->{ident} ne $ident
+    } @{ $user->other_profiles } ];
+    require Storable;
+    $user->meta( other_profiles => Storable::nfreeze( $profiles ) );
+    $user->save;
+}
+
+1;
---
+++ plugins/Elsewhere/lib/WWW/SocialNetworks.pm
@@ -0,0 +1,70 @@
+package WWW::SocialNetworks;
+use strict;
+use base qw( Class::Accessor::Fast );
+
+__PACKAGE__->mk_accessors( qw( type ) );
+
+our %Networks = (
+    '43things'  => [ '43Things', 'http://www.43things.com/person/%s/' ],
+    aim         => [ 'AIM', 'aim:goim?screenname=%s' ],
+    yahoo       => [ 'Yahoo! Messenger', 'http://edit.yahoo.com/config/send_webmesg?.target=%s' ],
+    skype       => [ 'Skype', 'callto://%s' ],
+    msn         => [ 'MSN Messenger', 'msnim:chat?contact=%s' ],
+    bebo        => [ 'Bebo', 'http://www.bebo.com/Profile.jsp?MemberId=%s' ],
+    catster     => [ 'Catster', 'http://www.catster.com/cats/%s' ],
+    delicious   => [ 'del.icio.us', 'http://del.icio.us/%s/' ],
+    digg        => [ 'Digg', 'http://digg.com/users/%s/' ],
+    dodgeball   => [ 'Dodgeball', 'http://www.dodgeball.com/user?uid=%s' ],
+    dogster     => [ 'Dogster', 'http://www.dogster.com/dogs/%s' ],
+    dopplr      => [ 'Dopplr', 'http://www.dopplr.com/traveller/%s/' ],
+    facebook    => [ 'Facebook', 'http://www.facebook.com/profile.php?id=%s' ],
+    flickr      => [ 'Flickr', 'http://flickr.com/photos/%s/' ],
+    goodreads   => [ 'GoodReads', 'http://www.goodreads.com/user/show/%s' ],
+    hi5         => [ 'Hi5', 'http://hi5.com/friend/profile/displayProfile.do?userid=%s' ],
+    jaiku       => [ 'Jaiku', 'http://%s.jaiku.com/' ],
+    lastfm      => [ 'Last.fm', 'http://www.last.fm/user/%s/' ],
+    linkedin    => [ 'LinkedIn', 'http://www.linkedin.com/in/%s/' ],
+    livejournal => [ 'LiveJournal', 'http://%s.livejournal.com/' ],
+    mog         => [ 'MOG', 'http://mog.com/%s' ],
+    multiply    => [ 'Multiply', 'http://%s.multiply.com/' ],
+    myspace     => [ 'MySpace', 'http://www.myspace.com/%s' ],
+    newsvine    => [ 'Newsvine', 'http://%s.newsvine.com/' ],
+    ning        => [ 'Ning', 'http://%s.ning.com/' ],
+    orkut       => [ 'Orkut', 'http://www.orkut.com/Profile.aspx?uid=%s' ],
+    pandora     => [ 'Pandora', 'http://pandora.com/people/%s' ],
+    pownce      => [ 'Pownce', 'http://pownce.com/%s/' ],
+    reddit      => [ 'Reddit', 'http://reddit.com/user/%s/' ],
+    sonicliving => [ 'SonicLiving', 'http://www.sonicliving.com/user/%s/' ],
+    stumbleupon => [ 'StumbleUpon', 'http://%s.stumbleupon.com/' ],
+    tabblo      => [ 'Tabblo', 'http://www.tabblo.com/studio/person/%s/' ],
+    tagworld    => [ 'TagWorld', 'http://www.tagworld.com/btrott' ],
+    technorati  => [ 'Technorati', 'http://technorati.com/people/technorati/%s' ],
+    tribe       => [ 'Tribe', 'http://people.tribe.net/%s' ],
+    twitter     => [ 'Twitter', 'http://twitter.com/%s' ],
+    upcoming    => [ 'Upcoming', 'http://upcoming.yahoo.com/user/%s' ],
+    youtube     => [ 'YouTube', 'http://www.youtube.com/user/%s' ],
+    zooomr      => [ 'Zooomr', 'http://www.zooomr.com/photos/%s' ],
+    vox         => [ 'Vox', 'http://%s.vox.com/' ],
+);
+
+sub all {
+    return \%Networks;
+}
+
+sub get {
+    my $class = shift;
+    my( $type ) = @_;
+    my $network = $class->SUPER::new;
+    $network->type( $type );
+    return $network;
+}
+
+sub name { return $Networks{ shift->type }[ 0 ] }
+
+sub uri_for {
+    my $network = shift;
+    my( $id ) = @_;
+    return sprintf $Networks{ $network->type }[ 1 ], $id;
+}
+
+1;
